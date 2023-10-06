7d4c8c6f42c58056f8389873dbca547daefde805
nchoin <139597297+nchoin@users.noreply.github.com>
2023-10-06 19:37:06 -0400

    tried to create FlexBox currently setting up with Grid

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
-var i = 0;
-var timerBox = document.querySelector('.timer');
+// var i = 0;
+// var timerBox = document.querySelector('.timer');
 
-//code for the timer. Change the var i = whatever I want the timer set to - how many 1 seconds. This connects to the 1000milliseconds below.
-var timer = setInterval(function(){
-    timerBox.textContent=i;
-    console.log(timerBox.textContent);
-    i--;
-    if(i<0){
-        //stop countdown use the variable you created mean the setInterval method.
-        clearInterval(timer);
-    }
-}, 1000);
+// //code for the timer. Change the var i = whatever I want the timer set to - how many 1 seconds. This connects to the 1000milliseconds below.
+// var timer = setInterval.function(){
+//     timerBox.textContent=i;
+//     console.log(timerBox.textContent);
+//     i--;
+//     if(i<0){
+//         //stop countdown use the variable you created mean the setInterval method.
+//         clearInterval(timer);
+//     }
+// }, 1000);
