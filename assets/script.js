248d42417367b479a2fad72045cb40a234f2e9d2
nchoin <139597297+nchoin@users.noreply.github.com>
2023-10-07 13:54:32 -0400

    add functions for show and hide sections

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ the homepage should show the following initially:
 1. Go to Quiz button
 2. Info about DDC
 3. Info about the quiz
+
 When I click the Continue to Quiz button Everthing on the initial page should switch to display None and the following should show up:
 1. The timer
 2. The start button
@@ -30,3 +31,63 @@ Everthing on the initial page should switch to display None and the following sh
 //         clearInterval(timer);
 //     }
 // }, 1000);
+
+//Function to make quiz frame and timer and counter/tally
+
+var quizAppear = document.getElementsByClassName("quizFrame");
+var tallyBoxAppear = document.getElementsByClassName("tallyCorrect");
+var statsAppear = document.getElementsByClassName("stats");
+var headerAppear = document.getElementsByClassName("start");
+var infoAppear = document.getElementsByClassName("infoBox");
+
+function showQuiz() {
+    if(quizAppear.style.display ==='none') {
+      quizAppear.style.display='show';
+    }
+    if(tallyBoxAppear.style.display === "none") {
+        tallyBoxAppear.style.display="show";
+    }
+}
+
+function hideQuiz() {
+    if(quizAppear.style.display ==='show') {
+        quizAppear.style.display='none';
+      }
+    if(tallyBoxAppear.style.display === "show") {
+          tallyBoxAppear.style.display="none";
+      }
+}
+
+function showStats(){
+    if(statsAppear.style.display ==='none') {
+        statsAppear.style.display='show';
+    }
+    if(tallyBoxAppear.style.display === "none") {
+        tallyBoxAppear.style.display="show";
+    }
+}
+function hideStats(){
+    if(statsAppear.style.display ==='show') {
+        statsAppear.style.display='none';
+    }
+    if(tallyBoxAppear.style.display === "show") {
+        tallyBoxAppear.style.display="none";
+    }
+}
+function showInfo(){
+    if(headerAppear.style.display ==='none') {
+        headerAppear.style.display='show';
+    }
+    if(infoAppear.style.display === "none") {
+        infoAppear.style.display="show";
+    }
+}
+function hideInfo(){
+    if(headerAppear.style.display ==='show') {
+        headerAppear.style.display='none';
+    }
+    if(infoAppear.style.display === "show") {
+        infoAppear.style.display="none";
+    }
+}
+
