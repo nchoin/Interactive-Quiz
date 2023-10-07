3a444e7be350c9cda24f50d2dd3cdabe995309a4
nchoin <139597297+nchoin@users.noreply.github.com>
2023-10-06 21:51:58 -0400

    updated the display for website, switched back to flexbox for display, changed class name for continue to quiz button. Added the title of the quiz to the first page.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
+/*
+the homepage should show the following initially:
+1. Go to Quiz button
+2. Info about DDC
+3. Info about the quiz
+When I click the Continue to Quiz button Everthing on the initial page should switch to display None and the following should show up:
+1. The timer
+2. The start button
+3. the quiz frame that says The quiz Title, and default to "question will be here" "answer choice will be here"
+4. The Next Button
+5. The Box with Tally of correct responses
+
+When the timer ends:
+Everthing on the initial page should switch to display None and the following should show up:
+1. Paragraph/box that says - You finished. Record your score and your Initials. 
+2. The Box with Tally of correct responses
+3. button to go to new page to complete the form.
+
+*/
 // var i = 0;
 // var timerBox = document.querySelector('.timer');
 
