d1135b532b76e0988833f76975f6a9942b4eb0b8
nchoin <139597297+nchoin@users.noreply.github.com>
2023-10-10 11:17:05 -0400

    attempted to add a reload to the stats page to clear the initials and score. Used setTimeout and location.reload.

--- assets/highScore.js
+++ assets/highScore.js
@@ -1,6 +1,12 @@
 const initialsToStore = document.getElementById('initialsScore');
 const scoreToStore = document.getElementById('score');
 
+function clearLabels(){
+    initialsToStore.textContent = '';
+    scoreToStore.textContent = '';
+}
+
 document.querySelector('#postScoreBtn').addEventListener('click',()=> {localStorage.setItem(initialsToStore.value, scoreToStore.value);
-document.getElementById('info').append("Score added🏆");    
+    document.getElementById('info').append("Score added🏆");  
+    location.reload();
 })
